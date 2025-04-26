798a1d870f77fc74ea946a51b1665978e8a4396b
Sahil Ijaz <hssahil2913@gmail.com>
2025-04-27 00:33:23 +0500

    validator required

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const validator = require("validator");
 const userSchema = new mongoose.Schema(
   {
     name: {
@@ -8,6 +8,7 @@ const userSchema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
+      validate: [validator.isEmail, "Provide correct email."],
       unique: true,
     },
     password: {
@@ -19,6 +20,31 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre(/^find/, function (next) {
+  this.find({
+    $and: [{ isDeleted: false }],
+  });
+  next();
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    this.password = await bcrypt.hash(this.password, 12);
+    this.confirmPassword = undefined;
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+userSchema.methods.checkPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
