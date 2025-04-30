15a5e80839b221c70b0403fe6140e384be21cac3
Sahil Ijaz <hssahil2913@gmail.com>
2025-04-30 09:16:30 +0500

    simple done

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}", "index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
