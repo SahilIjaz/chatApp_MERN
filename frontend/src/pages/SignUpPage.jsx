b18acf5360f0f0891d3f10d6171e1e99c7114da5
Sahil Ijaz <hssahil2913@gmail.com>
2025-05-05 15:01:41 +0500

    fivd

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -12,8 +12,22 @@ const SignUpPage = () => {
 const {signup,isSigningUp}=useAuthStore();
 
 const validateForm=()=>{}
+const handleSubmit=(e)=>{e.preventDefault()}
 
-return <>SignUpPage</>
+return <div className='min-h-screen grid lg:grid-clos-2'>
+
+<div className='flex flex-col justify-center items-center p-6 sm:p-12'>
+<div className='w-full max-w-md space-y-8'>  
+
+  
+</div>
+
+
+</div>
+
+
+</div>
+  
 }
 
 export default SignUpPage
