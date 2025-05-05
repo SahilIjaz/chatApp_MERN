1f0bfeb5aa756328bae8300eef64e75daa777b8a
Sahil Ijaz <hssahil2913@gmail.com>
2025-05-05 15:22:33 +0500

    signUp UI made

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -132,7 +132,7 @@ import { useAuthStore } from "../store/useAuthStore";
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from "lucide-react";
 import { Link } from "react-router-dom";
 
-import AuthImagePattern from "../components/AuthImagePattern";
+import AuthImagePattern from "../componenets/skeleton/AuthImagePattern";
 import toast from "react-hot-toast";
 
 const SignUpPage = () => {
